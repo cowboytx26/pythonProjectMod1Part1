89376268df4a93609268764562b0238b9f293f6b
Brian W Wiatrek <brianwiatrek@aol.com>
2024-06-08 12:22:49 -0500

    Added documentation

--- myinfo.py
+++ myinfo.py
@@ -1,7 +1,10 @@
 # Author:  Brian Wiatrek
 # Date written: 06/08/24
 # Assignment:   Module 1 exercise 1 part 1
-# Short Desc:   xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+# Short Desc:   This is a python program that will print name, address, and telephone number
+# strName: this variable contains name
+# strAddress: this variable contains address
+# strPhone: this variable contains phone number
 
 strName = input("What is your name?")
 strAddress = input("What is your address?")
