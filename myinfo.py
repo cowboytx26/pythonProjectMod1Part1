e7944cf16386cc26753bb900a5a26a214f7e4af2
Brian W Wiatrek <brianwiatrek@aol.com>
2024-06-08 11:56:39 -0500

    Initial commit

---
+++ myinfo.py
@@ -0,0 +1,10 @@
+# Author:  Brian Wiatrek
+# Date written: 06/08/24
+# Assignment:   Module 1 exercise 1 part 1
+# Short Desc:   xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+strName = input("What is your name?")
+strAddress = input("What is your address?")
+strPhone = input("What is your phone number?")
+
+print(strName, strAddress, strPhone, sep='\n')
